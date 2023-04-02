62c393c2b43d3ef603a03edf62c2da532f6b19ee
Liyze09 <a52990419@outlook.com>
2023-04-02 12:18:06 +0800

    Copper tools Damage and Speed Fix

--- src/main/java/net/liyze/usefuladditions/item/Items.java
+++ src/main/java/net/liyze/usefuladditions/item/Items.java
@@ -11,9 +11,9 @@ public class Items {
     public static final Item MINI_REDSTONE_BLOCK = new Item(new FabricItemSettings().group(ItemGroup.MISC));
     public static final Item MINI_LAPIS_LAZULI_BLOCK = new Item(new FabricItemSettings().group(ItemGroup.MISC));
     /*Copper Tools*/
-    public static final ToolItem COPPER_PICKAXE = new PickaxeItem(CopperToolMaterial.INSTANCE,0,1.2F,new FabricItemSettings().group(ItemGroup.TOOLS));
-    public static final ToolItem COPPER_SWORD = new SwordItem(CopperToolMaterial.INSTANCE,6,1.5F,new FabricItemSettings().group(ItemGroup.COMBAT));
-    public static final ToolItem COPPER_AXE = new AxeItem(CopperToolMaterial.INSTANCE,7,0.8F,new FabricItemSettings().group(ItemGroup.TOOLS));
-    public static final ToolItem COPPER_HOE = new CustomHoeItem(CopperToolMaterial.INSTANCE,2,1.0F,new FabricItemSettings().group(ItemGroup.TOOLS));
-    public static final ToolItem COPPER_SHOVEL = new ShovelItem(CopperToolMaterial.INSTANCE,-2,2.5F,new Item.Settings().group(ItemGroup.TOOLS));
+    public static final ToolItem COPPER_PICKAXE = new PickaxeItem(CopperToolMaterial.INSTANCE,0,-2.8F,new FabricItemSettings().group(ItemGroup.TOOLS));
+    public static final ToolItem COPPER_SWORD = new SwordItem(CopperToolMaterial.INSTANCE,3,-2.5F,new FabricItemSettings().group(ItemGroup.COMBAT));
+    public static final ToolItem COPPER_AXE = new AxeItem(CopperToolMaterial.INSTANCE,5,-3.2F,new FabricItemSettings().group(ItemGroup.TOOLS));
+    public static final ToolItem COPPER_HOE = new CustomHoeItem(CopperToolMaterial.INSTANCE,1,-3.0F,new FabricItemSettings().group(ItemGroup.TOOLS));
+    public static final ToolItem COPPER_SHOVEL = new ShovelItem(CopperToolMaterial.INSTANCE,-3,-1.5F,new Item.Settings().group(ItemGroup.TOOLS));
 }
