803529d85b90c5c4011cdf6993dd9daccb886e46
Liyze09 <a52990419@outlook.com>
2023-04-16 13:10:43 +0800

    ACCUMULATE

--- src/main/java/net/liyze/usefuladditions/worldgen/WorldgenConfiguration.java
+++ src/main/java/net/liyze/usefuladditions/worldgen/WorldgenConfiguration.java
@@ -1,6 +1,5 @@
 package net.liyze.usefuladditions.worldgen;
 
-import net.minecraft.block.Blocks;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.feature.*;
@@ -10,17 +9,19 @@ import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 
 import java.util.Arrays;
 
+import static net.liyze.usefuladditions.block.BlocksConfiguration.QUARTZITE_BLOCK;
+
 public class WorldgenConfiguration {
     public static ConfiguredFeature<?, ?> OVERWORLD_QUARTZITE_CONFIGURED_FEATURE = new ConfiguredFeature<>
             (Feature.ORE, new OreFeatureConfig(
                     OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
-                    Blocks.WHITE_WOOL.getDefaultState(),
+                    QUARTZITE_BLOCK.getDefaultState(),
                     12));
 
     public static PlacedFeature OVERWORLD_QUARTZITE_PLACED_FEATURE = new PlacedFeature(
             RegistryEntry.of(OVERWORLD_QUARTZITE_CONFIGURED_FEATURE),
             Arrays.asList(
-                    CountPlacementModifier.of(8),
+                    CountPlacementModifier.of(6),
                     SquarePlacementModifier.of(),
                     HeightRangePlacementModifier.uniform(YOffset.fixed(-5), YOffset.fixed(128))
             ));
